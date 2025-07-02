f21a62ac03e3b91e9d498383900909e357e44693
marbein <marbein@gmail.com>
2025-07-02 11:52:26 -0400

    Adiciona arquivo desafio03.js com conteúdo inicial

---
+++ desafio03.js
@@ -0,0 +1,28 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    if (this.tipo === "mago") {
+      ataque = "magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada";
+    } else if (this.tipo === "monge") {
+      ataque = "artes marciais";
+    } else if (this.tipo === "ninja") {
+      ataque = "shuriken";
+    } else {
+      ataque = "um ataque desconhecido";
+    }
+
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+  }
+}
+
+const heroi1 = new Heroi("jose", 35, "ninja");
+heroi1.atacar();
